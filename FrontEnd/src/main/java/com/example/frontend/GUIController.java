6c9771553abe247cb25822229af69769c954b12f
eliosoFH <wi23b100@technikum-wien.at>
2025-05-07 21:02:17 +0200

    JavaFX fertig

--- FrontEnd/src/main/java/com/example/frontend/GUIController.java
+++ FrontEnd/src/main/java/com/example/frontend/GUIController.java
@@ -4,19 +4,61 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import javafx.application.Platform;
+
+import java.net.URI;
 
 public class GUIController {
     @FXML
-    private Label welcomeText;
-    public TextField tfNum1;
-    public TextField tfNum2;
+    public TextField tfId;
     public Label labelResult;
 
 
     public void onCurrentClick(ActionEvent actionEvent) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/energy/current"))
+                    .GET()
+                    .build();
 
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenAccept(response -> {
+                        Platform.runLater(() -> labelResult.setText("Current: " + response));
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+            labelResult.setText("Fehler beim Laden!");
+        }
     }
 
     public void onHistoricalClick(ActionEvent actionEvent) {
+        String id = tfId.getText(); // z. B. „2“
+        if (id == null || id.isEmpty()) {
+            labelResult.setText("Bitte ID eingeben");
+            return;
+        }
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/energy/historic/" + id))
+                    .GET()
+                    .build();
+
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenAccept(response -> {
+                        Platform.runLater(() -> labelResult.setText("Historic: " + response));
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+            labelResult.setText("Fehler beim Laden!");
+        }
     }
 }
--- FrontEnd/src/main/java/module-info.java
+++ FrontEnd/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.frontend {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
 
 
     opens com.example.frontend to javafx.fxml;
