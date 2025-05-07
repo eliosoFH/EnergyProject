d4377542fa72881b31ff6d5c4e0316ae23750650
eliosoFH <wi23b100@technikum-wien.at>
2025-05-07 20:44:24 +0200

    JavaFX

---
+++ FrontEnd/src/main/java/com/example/frontend/GUIController.java
@@ -0,0 +1,22 @@
+package com.example.frontend;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class GUIController {
+    @FXML
+    private Label welcomeText;
+    public TextField tfNum1;
+    public TextField tfNum2;
+    public Label labelResult;
+
+
+    public void onCurrentClick(ActionEvent actionEvent) {
+
+    }
+
+    public void onHistoricalClick(ActionEvent actionEvent) {
+    }
+}
