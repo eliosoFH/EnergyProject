9fcbead83fc3a908f77438324c2695d2e097c8a1
eliosoFH <wi23b100@technikum-wien.at>
2025-05-07 17:34:52 +0200

    Initial commit with FrontEnd and BackEndAPI

---
+++ BackEndAPI/src/test/java/com/example/BackEndAPI/BackEndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackEndAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
