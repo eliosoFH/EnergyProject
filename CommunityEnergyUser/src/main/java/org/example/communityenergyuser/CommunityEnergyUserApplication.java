7850e0d9a30bf6e4f197ea71e5297a8ada3cc52f
Laurin-wi22b071 <135732799+Laurin-wi22b071@users.noreply.github.com>
2025-06-16 13:15:17 +0200

    CommunityEnergyUser added

---
+++ CommunityEnergyUser/src/main/java/org/example/communityenergyuser/CommunityEnergyUserApplication.java
@@ -0,0 +1,13 @@
+package org.example.communityenergyuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityEnergyUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityEnergyUserApplication.class, args);
+    }
+
+}
---
+++ CommunityEnergyUser/src/test/java/org/example/communityenergyuser/CommunityEnergyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.communityenergyuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityEnergyUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
