213936aefae890f975d4a20c34780d1f78c4f8c3
Laurin-wi22b071 <135732799+Laurin-wi22b071@users.noreply.github.com>
2025-06-16 19:27:11 +0200

    CommunityEnergyProducer added

---
+++ CommunityEnergyProducer/src/main/java/org/example/communityenergyproducer/CommunityEnergyProducerApplication.java
@@ -0,0 +1,13 @@
+package org.example.communityenergyproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityEnergyProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityEnergyProducerApplication.class, args);
+    }
+
+}
---
+++ CommunityEnergyUser/src/main/java/org/example/communityenergyuser/Controller/EnergyUserController.java
@@ -0,0 +1,4 @@
+package org.example.communityenergyuser.Controller;
+
+public class EnergyUserController {
+}
