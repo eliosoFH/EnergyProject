2506d68bae6f0dacb6683730bf11ebc9aba4c16f
eliosoFH <wi23b100@technikum-wien.at>
2025-06-19 17:02:12 +0200

    Usage Service fertig (MessageQueue outgoing noch zu überprüfen

--- UsageService/src/main/java/org/example/usageservice/message/MessageListener.java
+++ UsageService/src/main/java/org/example/usageservice/message/MessageListener.java
@@ -6,27 +6,36 @@ import org.example.usageservice.dto.EnergyStats;
 import org.example.usageservice.repository.EnergyUsageHourlyEntity;
 import org.example.usageservice.repository.EnergyUsageDatabaseRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 import java.sql.SQLOutput;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class MessageListener {
 
     private final EnergyUsageDatabaseRepository repository;
-    public MessageListener(EnergyUsageDatabaseRepository repository) {
+    private final RabbitTemplate rabbit;
+    public MessageListener(EnergyUsageDatabaseRepository repository, RabbitTemplate rabbit) {
         this.repository = repository;
+        this.rabbit = rabbit;
     }
 
     List<EnergyStats> list = new ArrayList<>();
 
     boolean producedHourOver = false;
     boolean usedHourOver = false;
+
+    double excessProduced = 0;
+    double excessUsed = 0;
 
     private EnergyStats energyStats = new EnergyStats();
 
@@ -37,21 +46,17 @@ public class MessageListener {
         try {
             EnergyStats data = objectMapper.readValue(message, EnergyStats.class);
 
-            if(energyStats.getTimestamp() == null) {
-                energyStats.setTimestamp(data.getTimestamp().withMinute(0).withSecond(0).withNano(0));
-            }
+            initTime(data.getTimestamp());
 
             if(data.getTimestamp().getHour() == energyStats.getTimestamp().getHour()) {
                 energyStats.setCommunityProduced(energyStats.getCommunityProduced()+ data.getCommunityProduced());
             } else {
+                excessProduced += data.getCommunityProduced();
                 producedHourOver = true;
                 if (usedHourOver && producedHourOver) {
                     postDB();
                 }
             }
-
-
-
 
         } catch (Exception e) {
             System.out.println("Fehler aufgetreten!");
@@ -66,13 +71,12 @@ public class MessageListener {
         try {
             EnergyStats data = objectMapper.readValue(message, EnergyStats.class);
 
-            if(energyStats.getTimestamp() == null) {
-                energyStats.setTimestamp(data.getTimestamp().withMinute(0).withSecond(0).withNano(0));
-            }
+            initTime(data.getTimestamp());
 
             if (data.getTimestamp().getHour() == energyStats.getTimestamp().getHour()) {
                 energyStats.setCommunityUsed(energyStats.getCommunityUsed() + data.getCommunityUsed());
             } else {
+                excessUsed += data.getCommunityUsed();
                 usedHourOver = true;
                 if (usedHourOver && producedHourOver) {
                     postDB();
@@ -88,6 +92,32 @@ public class MessageListener {
         energyStats.setGritUsed(energyStats.getCommunityUsed()- energyStats.getCommunityProduced());
         EnergyUsageHourlyEntity sql = new EnergyUsageHourlyEntity(energyStats.getTimestamp().plusHours(1), energyStats.getCommunityUsed(), energyStats.getCommunityProduced());
         repository.save(sql);
+
+        try {
+            // send energyStats to queue
+            Map<String, Object> messageMap = new HashMap<>();
+            messageMap.put("produced", energyStats.getCommunityProduced());
+            messageMap.put("used", energyStats.getCommunityUsed());
+            messageMap.put("grid", energyStats.getGritUsed());
+            messageMap.put("timestamp", energyStats.getTimestamp().plusHours(1));
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String messageJson = objectMapper.writeValueAsString(messageMap);
+            rabbit.convertAndSend("update");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         energyStats = new EnergyStats();
+        energyStats.setCommunityUsed(excessUsed);
+        energyStats.setCommunityProduced(excessProduced);
+        excessUsed = 0;
+        excessProduced = 0;
+    }
+
+    private void initTime(LocalDateTime date) {
+        if(energyStats.getTimestamp() == null) {
+            energyStats.setTimestamp(date.withMinute(0).withSecond(0).withNano(0));
+        }
     }
 }
