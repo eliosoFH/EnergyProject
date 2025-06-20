63db8da8c291e2bb9a994e830a8372ce3964390f
eliosoFH <wi23b100@technikum-wien.at>
2025-06-20 22:05:07 +0200

    CurrentEnergyPercentage started

---
+++ CurrentEnergyPercentage/src/main/java/com/example/currentenergypercentage/CurrentEnergyPercentageApplication.java
@@ -0,0 +1,13 @@
+package com.example.currentenergypercentage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentEnergyPercentageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrentEnergyPercentageApplication.class, args);
+    }
+
+}
---
+++ CurrentEnergyPercentage/src/test/java/com/example/currentenergypercentage/CurrentEnergyPercentageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currentenergypercentage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentEnergyPercentageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
